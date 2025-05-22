8d570855ea8ab75dc2c95ba544bc1dd46e7883e5
Andrea Nolasco <ant.andreansc@gmail.com>
2025-05-22 03:10:36 +0000

    Termine proyecto

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Style.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [tarea, setTarea] = useState("");
+	const [todos, setTodos] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const clickEnter = (event) => {
+		if (event.key === "Enter" && tarea.trim() !== "") {
+			setTodos([...todos, tarea]);
+			setTarea("");
+		}
+	};
+
+	const eliminarTarea = (index) => {
+		const nuevasTareas = todos.filter((_, i) => i !== index);
+		setTodos(nuevasTareas);
+	};
+
+	return (
+		<div className="text-center container">
+			<h1 className="text-center mt-5 titulo">todos</h1>
+
+			<div className="d-flex justify-content-center">
+				<input
+					className="form-control w-50"
+					placeholder="Ingresa una nueva tarea"
+					value={tarea}
+					onChange={(event) => setTarea(event.target.value)}
+					onKeyDown={clickEnter}
+				/>
+
+
+			</div>
+
+			<div className="d-flex justify-content-center">
+				<ul className="list-group mt-3 w-50">
+					{todos.map((item, index) => (
+						<li
+							key={index}
+							className="list-group-item d-flex justify-content-between align-items-center tarea-item"
+						>
+							<span>{item}</span>
+							<button
+								className="btn btn-sm eliminar-btn"
+								onClick={() => eliminarTarea(index)}
+							>
+								X
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+
 		</div>
 	);
 };
 
 export default Home;
+
